5724d616bbf09984adf670612adf728d1ce8be5b
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-10-02 09:51:38 +0300

    Revert "checking commit"
    This reverts commit 0597e05cc213b1dfa70070c9f69b3df268b8af6a.

--- app/src/main/java/com/example/birthday_card/MainActivity.kt
+++ app/src/main/java/com/example/birthday_card/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.birthday_card.ui.theme.BirthdaycardTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) { f
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BirthdaycardTheme {
